5bcd2d97a1e25c066242778c3e55025f9f11b706
Juankz <Juanvelandia@protonmail.com>
2017-06-29 14:59:00 -0500

    Añadir señales de control con salida digital

--- src/control.cpp
+++ src/control.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 
 #define DEBUG 0
+
 const int S1_S2 = 4;
 const int S3_S4 = 5;
 const int S5_S5 = 6;
@@ -29,6 +30,7 @@ long actual_t1;
 
 /* Trifasica */
 int senales[6]; // Secuencia: ACN - CNB - ANB - ANC - CBN - ABN
+int pines_senales[] = {7,8,9,10,11,12};
 int senal_actual = 6;
 
 boolean cruce_detectado = false;
@@ -40,7 +42,14 @@ float alfaMicroseconds; //valor en us
 float anchoPulso = 0.07*T; // Valor en us
 float dif_pulsos_us=(DIF_PULSOS_GRADOS/float(360*60))*1000000;
 
-int valor_salida = LOW;
+void apagar_todo(){
+  for(int i = 0; i < 6;i++){
+    if(senales [i] != LOW){
+      senales[i]=LOW;
+      digitalWrite(pines_senales[i], LOW);
+    }
+  }
+}
 
 int obtener_prox_senal(int s){
   if(s >= 5){
@@ -54,6 +63,7 @@ void cruce(){
   actual_t1 = micros();
   /*Aseguramos que solo tome el cruce de un semiciclo*/
   if(actual_t1-ultimo_t1>T*1.5){
+    apagar_todo();
     ultimo_t1 = micros();
     cruce_detectado = true;
     estado = ESPERAR_ALFA;
@@ -67,6 +77,9 @@ void setup(){
   #endif
   pinMode(pinSalida,OUTPUT);
   pinMode(pinCrucex0, INPUT_PULLUP);
+  for(int i = 0; i < 6; i++){
+    pinMode(pines_senales[i],OUTPUT);
+  }
   attachInterrupt(digitalPinToInterrupt(pinCrucex0), cruce, RISING);
   ultimo_t1 = micros();
 }
@@ -93,37 +106,55 @@ void loop(){
 
   if(cruce_detectado_copia){
     actual_t0 = micros();
+    int prox_senal = obtener_prox_senal(senal_actual);
+
     switch (estado) {
+
+      /*Una vez que llegue al alfa, escribir las dos señales en alto*/
       case ESPERAR_ALFA:
         if(actual_t0 - ultimo_t0 > alfaMicroseconds){
-          digitalWrite(pinSalida,HIGH);
+          senales [senal_actual] = HIGH;
+          senales [prox_senal] = HIGH;
+          digitalWrite(pines_senales[senal_actual],HIGH);
+          digitalWrite(pines_senales[prox_senal],HIGH);
           ultimo_t0 = micros();
           estado=DISPARANDO;
         }
         break;
 
+      /* Cuando el tiempo ha sobrepasado el ancho del pulso, apaga las señales y continúa con la
+         siguiente señal. Si ya ha disparado todas las señales, salga y espera al siguiente cruce por cero
+      */
       case DISPARANDO:
         if(actual_t0 - ultimo_t0 > anchoPulso){
-          digitalWrite(pinSalida,LOW);
-          estado = ESPERANDO_SIG_PULSO;
+          senales [senal_actual] = LOW;
+          senales [prox_senal] = LOW;
+          digitalWrite(pines_senales[senal_actual],LOW);
+          digitalWrite(pines_senales[prox_senal],LOW);
+          senal_actual=prox_senal;
+          if(senal_actual<5){
+            estado = ESPERANDO_SIG_PULSO;
+          }else{
+            estado = APAGAR;
+          }
         }
         break;
 
       case ESPERANDO_SIG_PULSO:
         if(actual_t0 - ultimo_t0 > dif_pulsos_us){
-          digitalWrite(pinSalida,HIGH);
-          ultimo_t0 = micros();
-          estado=APAGAR;
+          senales [senal_actual] = HIGH;
+          senales [prox_senal] = HIGH;
+          digitalWrite (pines_senales[senal_actual],HIGH);
+          digitalWrite (pines_senales[prox_senal],HIGH);
+          ultimo_t0 = micros ();
+          estado = DISPARANDO;
         }
         break;
 
       case APAGAR:
-        if(actual_t0 - ultimo_t0 > anchoPulso){
-          digitalWrite(pinSalida,LOW);
-          /* Salir */
-          cruce_detectado=false;
-          estado = ESPERAR_ALFA;
-        }
+        /* Salir */
+        cruce_detectado=false;
+        estado = ESPERAR_ALFA;
         break;
 
       default:
